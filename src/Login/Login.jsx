9c5b2ae5486b12a97187a5957cbc51680a4d9bb8
elisaul batista <elisaulbatista@pursuit.org>
2023-12-18 21:20:26 -0500

    added log in

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
-import './login.css'
+import React, { useState } from 'react';
+import './login.css';
+
 const Login = () => {
-    return (
-        <div>
-            <h2>this is login</h2>
-        </div>
-    );
-}
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add logic same as registration when server is working
+    console.log('Form Data:', formData);
+
+    
+    setFormData({
+      username: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
